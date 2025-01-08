6ef28125e8883f004975ae60bb224032fc56e279
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-08 19:20:38 +0100

    Add files via upload

---
+++ io.h
@@ -0,0 +1,25 @@
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+#endif
+
+static inline void outb(uint8_t data, uint16_t port){
+	asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
+}
+static inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+static inline uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	asm volatile("inw %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+static inline void outw(uint16_t port, uint16_t data) {
+	asm volatile("outw %0, %1" : : "a"(data), "Nd"(port));
+}
