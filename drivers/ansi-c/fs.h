78ffd2362282cf0ac1a1b53e1cec65f21b620f74
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2025-01-10 10:28:34 +0100

    Update fs.h

--- drivers/ansi-c/fs.h
+++ drivers/ansi-c/fs.h
@@ -3,7 +3,10 @@
 
 #include "disk.h"
 
-#define FILE_NAME_MAX 64
+#define PROTECTED_BLOCKS ((uint32_t)SIZE/512)//size of the os. SIZE / 512
+#define MAX_FILES 1000//the max number of files that the filesystem can handle
+#define MAX_SECTORS ((uint32_t)8574934592) //near 8  gb of memory
+#define FILE_NAME_MAX 51
 #define NUM_BLOCKS (DISK_SIZE / BLOCK_SIZE)
 
 typedef struct {
@@ -11,7 +14,7 @@ typedef struct {
     unsigned int start_block;
     unsigned int length;
 } File;
-
+int search_file();
 void fs_init();
 int fs_create_file(const char* name, unsigned int size);
 int fs_read_file(const char* name, char* buffer);
